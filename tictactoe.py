3083d661c80724a675422ddb134711821f96d80f
fonglh <fonglh@gmail.com>
2016-02-17 23:34:33 +0800

    Add tictactoe game skeleton.
    Adapted from
    https://estl.gitbooks.io/moe-computing-course/content/chapters/2-abstraction.html
    
    Students are expected to stub and implement the functions.

---
+++ tictactoe.py
@@ -0,0 +1,18 @@
+def tictactoe():
+    """
+    Human plays a single Tic-Tac-Toe game against an AI
+    """
+    game = new_game()
+    humans_turn = get_starting_player()
+    game_result = None
+
+    while game_result is None:
+        if humans_turn:
+            game = humans_move(game)
+            humans_turn = False
+        else:
+            game = computers_move(game)
+            humans_turn = True
+        game_result = check_winner(game)
+    print(game_result)
+
