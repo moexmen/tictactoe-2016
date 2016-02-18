fa69d3ba51ab9d56f503bbfeea164405462d638d
fonglh <fonglh@gmail.com>
2016-02-18 09:07:59 +0800

    Add function stubs and a TODO item.
    Need function stubs to act as placeholders to reduce the change of merge
    conflicts.

--- tictactoe.py
+++ tictactoe.py
@@ -20,3 +20,26 @@ def new_game():
     """Returns a representation of an empty game grid"""
     pass
 
+def humans_move(game_board):
+    """
+    Displays the game board to the user, then
+    gets the human player's move and executes it.
+    """
+    pass
+
+def computers_move(game_board):
+    """Executes the computers player's move if empty cell exists"""
+    pass
+
+def check_winner(game_board):
+    """
+    Checks if there is a winner or if it is a draw.
+    Returns one of the following strings as appropriate:
+    - "Human Wins!"
+    - "Computer Wins!"
+    - "Draw"
+    """
+    pass
+
+# TODO: add missing stub
+
