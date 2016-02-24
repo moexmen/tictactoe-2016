1756a43d8a520de331bdfccb22fb07c2e571ef87
Thamwl <tham_wai_leng@moe.edu.sg>
2016-02-24 07:18:33 +0000

    3 test for computer move

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -22,7 +22,13 @@ class TestGamePlay(unittest.TestCase):
             self.assertEqual(humans_move(game), expected)
 
     def test_computers_move(self):
-        self.fail("not implemented")
+        self.assertEqual(computer_move("EOEXXEEEE"),"OOEXXEEEE")
+	
+    def test_computer_move_1(self): 
+	self.assertEqual(computer_move(""XOOEXXEXE"),"XOOOXXEXE")
+
+    def test_computer_move_2(self):
+	self.assertEqual(computer_move(""XOOXXXOXE"),"XOOXXXOXO")
 
     def test_check_winner(self):
         self.fail("not implemented")
